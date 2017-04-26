df27b8508477f0e6989a7143339bc3e9bded5453
Jesus Gómez <jesus_jgb_45@hotmail.com>
2017-04-27 00:09:47 +0200

    Layout Component

---
+++ src/app/reducers/layout/index.js
@@ -0,0 +1,16 @@
+import { TOOGLE_VISIBILITY_ASIDE } from './actionTypes';
+export * from './actions';
+
+function layout(state = {
+    visibilityAside: true
+}, action) {
+    switch (action.type) {
+        case TOOGLE_VISIBILITY_ASIDE:
+            const visibilityAside = !state.visibilityAside;
+            return Object.assign({}, state, { visibilityAside });
+        default:
+            return state;
+    }
+}
+
+export default layout;
