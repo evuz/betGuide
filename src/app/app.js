9b5c397a4586d1ef645cd3f073857be7548ca164
Jesus Gómez <jesus_jgb_45@hotmail.com>
2017-04-27 00:10:44 +0200

    Integrate layout component

--- src/app/app.js
+++ src/app/app.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
+import Aside from './components/Aside';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Layout from './components/Layout';
+
+import './app.scss';
 
 class App extends Component {
     render() {
         return (
-            <div>Hello World</div>
+            <Layout
+                aside={<Aside />}
+                footer={<Footer />}
+                header={<Header />}
+            >
+            Hola
+            </Layout>
         );
     }
 }
