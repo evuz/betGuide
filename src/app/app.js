40ed106e16e1a3315359faab537e2f3e7235df1e
evuz <jesus_jgb_45@hotmail.com>
2017-05-03 13:43:39 +0200

    SignIn app when has token

--- src/app/app.js
+++ src/app/app.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import Aside from './components/Aside';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Layout from './components/Layout';
 import Login from './components/Login';
 
+import { setUser } from './reducers/login';
+
 import './app.scss';
 
 class App extends Component {
+    componentWillMount() {
+        if (localStorage.token) {
+            fetch('http://localhost:3001/api/signin', {
+                method: 'GET',
+                headers: {
+                    'Authorization': 'Bearer ' + localStorage.token
+                },
+            })
+                .then(res => res.json())
+                .then(res => {
+                    this.props.setUser(res.user);
+                })
+        }
+    }
     render() {
         return (
             <Layout
@@ -19,4 +36,8 @@ class App extends Component {
     }
 }
 
-export default App;
+const mapDispatchToProps = {
+    setUser
+}
+
+export default connect(null, mapDispatchToProps)(App);
