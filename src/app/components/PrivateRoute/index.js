f946c424bb959ee21d5c4081473068ea69d3a58a
evuz <jesus_jgb_45@hotmail.com>
2017-05-05 12:02:44 +0200

    PrivateRoute component

---
+++ src/app/components/PrivateRoute/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Redirect, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+const PrivateRoute = (props) => {
+    const { user, component: Component, path, initApp} = props;
+    console.log(props);
+    return (
+        !initApp ? null : <Route path={path} render={props => (
+            user.email ? (
+                <Component />
+            ) : (
+                    <Redirect to={{
+                        pathname: '/'
+                    }} />
+                )
+        )} />
+    )
+}
+
+const mapStateToProps = state => ({
+    user: state.login.user,
+    initApp: state.app.initApp
+})
+
+export default connect(mapStateToProps)(PrivateRoute);
