d48727083ee389c924a64f6276662dcfc7809a28
Jesus Gómez <jesus_jgb_45@hotmail.com>
2017-05-06 19:36:35 +0200

    fetch stats

--- src/app/views/global/index.js
+++ src/app/views/global/index.js
@@ -1,14 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import Header from '../../components/Header';
 import Layout from '../../components/Layout';
+import StatsDetails from '../../components/StatsDetails';
+
+import { setUserStats } from '../../reducers/stats';
 
 import './index.scss';
 
-const GlobalView = () => (
-    <Layout header={<Header />} >
-        <h1>GlobalView</h1>
-    </Layout>
-)
+class GlobalView extends Component {
+    componentWillMount() {
+        fetch('http://localhost:3001/api/userstats', {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Bearer ' + localStorage.token
+            },
+        })
+            .then(res => res.json())
+            .then(res => {
+                if (res.error) return;
+                this.props.setUserStats(res.payload);
+            })
+    }
 
-export default GlobalView;
+    render() {
+        const {stats} = this.props;
+        return (
+            <Layout header={<Header />} >
+                <StatsDetails/>
+            </Layout>
+        );
+    }
+}
+
+const mapStateToProps = state => ({
+    stats: state.stats
+})
+
+const mapDispatchToProps = {
+    setUserStats
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(GlobalView);
