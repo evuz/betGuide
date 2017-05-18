4d90a5d0f8641c071dba635e5014a95ea58a7477
evuz <jesus_jgb_45@hotmail.com>
2017-05-18 17:46:22 +0200

    Add react-tap-event-plugin

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import App from './app/app';
 
 import configureStore from './configureStore';
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
 
 const { store, history } = configureStore();
 
