335bcbc0a350dfa8347264ba8fe547566f8c5bf9
evuz <jesus_jgb_45@hotmail.com>
2017-05-18 14:49:27 +0200

    Add Eslint

--- webpack.config.js
+++ webpack.config.js
@@ -4,33 +4,41 @@ var entryPath = path.join(__dirname, 'src');
 var outPath = path.join(__dirname, 'dist');
 
 module.exports = {
-    context: entryPath,
-    entry: './index.js',
-    output: {
-        path: outPath,
-        filename: 'bundle.js'
-    },
-    devtool: 'source-map',
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                options: { presets: ['es2015', 'react'] },
-                exclude: /node_modules/
-            },
-            {
-                test: /\.(sass|scss)$/, //Check for sass or scss file names
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader',
-                ]
-            }
+  context: entryPath,
+  entry: './index.js',
+  output: {
+    path: outPath,
+    filename: 'bundle.js'
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+        options: {
+          // eslint options (if necessary)
+        },
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        options: { presets: ['es2015', 'react'] },
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(sass|scss)$/, //Check for sass or scss file names
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
         ]
-    },
-    devServer: {
-        historyApiFallback: true,
-        contentBase: outPath
-    }
+      }
+    ]
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: outPath
+  }
 }
