9943f2ff3762747fcf4fd9d10148dfb55a800044
evuz <jesus_jgb_45@hotmail.com>
2017-05-19 13:46:18 +0200

    StatsDetails component: chart I

--- src/app/components/StatsDetails/StatsDetailsItem/index.js
+++ src/app/components/StatsDetails/StatsDetailsItem/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Card, CardText, CardHeader } from 'material-ui';
+import { PieChart, Pie, Cell } from 'recharts';
 import './index.scss';
 
 const StatsDetailsItem = (props) => {
@@ -7,15 +8,34 @@ const StatsDetailsItem = (props) => {
   const date = new Date(parseInt(id, 10));
   const monthStr = date.toLocaleDateString('en', { month: 'long' });
 
+  const data = [{ name: 'Group A', value: 400 }, { name: 'Group B', value: 300 },
+  { name: 'Group C', value: 300 }, { name: 'Group D', value: 200 }];
+  const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+
   return (
     <div className="stats_details_component_item">
-      <Card>
+      <Card style={style.card}>
         <CardHeader
           title={`${monthStr} ${date.getFullYear()}`}
         />
         <CardText>
+          <PieChart width={218} height={180} >
+            <Pie
+              data={data}
+              cx={110}
+              cy={85}
+              innerRadius={60}
+              outerRadius={80}
+              fill="#8884d8"
+              paddingAngle={5}
+            >
+              {
+                data.map((entry, index) => <Cell fill={COLORS[index % COLORS.length]} />)
+              }
+            </Pie>
+          </PieChart>
           <p>{profits}</p>
-          <p>{winPicks + lostPicks + voidPicks}</p>
+          <p>{`${winPicks} ${lostPicks}  ${voidPicks}`}</p>
         </CardText>
       </Card>
       {
@@ -25,6 +45,12 @@ const StatsDetailsItem = (props) => {
       }
     </div>
   );
+};
+
+const style = {
+  card: {
+    width: '250px',
+  },
 };
 
 export default StatsDetailsItem;
